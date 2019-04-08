751e50cb4d30889f2898e6547b5ac8244ef68a95
cterrasid <clarette.terrasi@gmail.com>
2019-04-08 18:30:27 +0200

    Add comparing function to interactions archive

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 //desarrollar una página web con un juego de "Adivinar el número".
 
 //genera un número al azar entre 1 y 100, y el jugador tiene que adivinarlo. 
-function getRandomNumber() {
+const getRandomNumber = function getRandomNumber() {
     return Math.ceil(Math.random() * 100);
 }
-console.log('> ' + getRandomNumber());
+
 
 //LLAMO AL INPUT Y LO GUARDO EN UNA VARIABLE
 const inputEl = document.querySelector('.game__input');
@@ -16,13 +16,26 @@ const buttonEl = document.querySelector('.game__button');
 const counterEl = document.querySelector('.game__counter');
 //LLAMO AL FEEDBACK Y LO GUARDO EN UNA VARIABLE
 const feedbackEl = document.querySelector('.game__feedback');
-//LLAMO AL NUMERO GANADOR
 
 //ESCUCHO AL CLICK DEL BOTON
-//MUESTRO EL NUMERO DE CLICKS EN EL CONTADOR
-//MANEJO EL VALOR DEL INPUT (KEYUP)
-//SI EL VALOR DEL INPUT ES MUY ALTO, MUESTRO MENSAJE1 EN EL FEEDBACK
-//SI EL VALOR DEL INPUT ES MUY BAJPO, MUESTRO MENSAJE2 EN EL FEEDBACK
-//SI ACIERTA EL NUMERO, MUESTRO MENSAJE3 EN EL FEEDBACK
-
-// En la parte principal, el jugador introduce un número y da al botón de probar. En la parte azul aparecen las pistas sobre si es muy alto o bajo, o si lo ha conseguido. En rojo en la esquina superior derecha aparece el número de intentos realizados. Cuando consigue acertar, el juego termina.
+buttonEl.addEventListener('click', handleButtonClick);
+ 
+//3. MANEJO EL VALOR DEL INPUT
+function handleButtonClick() {
+    const inputValue = inputEl.value;
+    //LO MUESTRO EN LA CONSOLA
+    console.log(inputValue);
+}
+//4. COMPARAR CON EL NUMERO ALEATORIO
+function name() {
+    if (inputValue === getRandomNumber) {
+        feedbackEl.innerHTML = (`¡Enhorabuena! ${inputValue} es el número ganador`)
+    } else if (inputValue > getRandomNumber) {
+        
+    }{
+        
+    }
+}
+    //SI EL VALOR DEL INPUT ES MUY ALTO, MUESTRO MENSAJE1 EN EL FEEDBACK
+    //SI EL VALOR DEL INPUT ES MUY BAJPO, MUESTRO MENSAJE2 EN EL FEEDBACK
+    //SI ACIERTA EL NUMERO, MUESTRO MENSAJE3 EN EL FEEDBACK
