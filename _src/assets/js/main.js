8e0122ed682d95114b31104e15e791300e41dabd
cterrasid <clarette.terrasi@gmail.com>
2019-04-16 12:42:14 +0200

    Add function for invalid numbers

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,39 +1,55 @@
 'use strict';
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
 function getInnerHTMLFeedback(feedback) {
-    const feedbackEl = document.querySelector('.game__feedback');
-    return feedbackEl.innerHTML = feedback;
+  const feedbackEl = document.querySelector('.game__feedback');
+  return feedbackEl.innerHTML = feedback;
 }
 
 const inputEl = document.querySelector('.game__input');
 const buttonEl = document.querySelector('.game__button');
 const randomNumber = getRandomNumber(100);
+
 function showFeedbackToUser() {
-    const inputValue = parseInt(inputEl.value);
+  const inputValue = parseInt(inputEl.value);
+  for (let inputValue = 0; inputValue <= 100; inputValue++) {
     if (inputValue === randomNumber) {
-        getInnerHTMLFeedback(`¡Excelente! ${inputValue} es el número ganador`);
+      getInnerHTMLFeedback(`¡Excelente! ${inputValue} es el número ganador`);
     } else if (inputValue > randomNumber) {
-        getInnerHTMLFeedback(`${inputValue} es un número muy alto`)
+      getInnerHTMLFeedback(`${inputValue} es un número muy alto`);
     } else if (inputValue < randomNumber) {
-        getInnerHTMLFeedback(`${inputValue} es un número muy bajo`)   
+      getInnerHTMLFeedback(`${inputValue} es un número muy bajo`);
     }
+  }
+  if (inputValue > 100) {
+    getInnerHTMLFeedback('');
+    alert(`${inputValue} no es un número válido`);
+  } else if (inputValue < 0) {
+    getInnerHTMLFeedback('');
+    alert(`${inputValue} no es un número válido`);
+  }
 }
 
 const counterEl = document.querySelector('.game__counter');
 let counterValue = 0;
 function countUserClicks() {
-    counterValue += 1;
-    counterEl.innerHTML = counterValue;
+  counterValue += 1;
+  counterEl.innerHTML = counterValue;
 }
 
+function handleInputValue() {
+  const emptyInput = '';
+  inputValue === emptyInput;
+}
 
 function handleButtonClick() {
-    showFeedbackToUser();
-    countUserClicks();
+  showFeedbackToUser();
+  countUserClicks();
+  //   handleInputValue();
 }
 
 buttonEl.addEventListener('click', handleButtonClick);
+// inputEl.addEventListener('blur', handleInputValue);
